988aef7b157e6e4c223d039d729f6cb5209f7df1
Anna Shelby <bonanashelby@gmail.com>
2017-03-15 11:10:43 -0700

    added layout.css modules.css projectInfo.js files worked on more css elements

--- app.js
+++ app.js
@@ -2,9 +2,18 @@
 
 var projects = [];
 
-// projects need a title, what language they are, the url and small description
+// projects need a title, what language they are linked as the url, completed on date and small description
 function Projects (projs) {
   this.title = projs.title;
   this.language = projs.language;
-  this.projectUrl = projs.projectUrl
+  this.projectUrl = projs.projectUrl;
+  this.completedOn = projs.completedOn;
+  this.description = projs.description;
 }
+
+// Projects.prototype.toHtml = function(){
+  // var $newProject = $('project.template').clone();
+  // $newProject.removeClass('template');
+
+  // if ()
+// };
