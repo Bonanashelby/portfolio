d8de588b422e52bb7851f31c1340a50186cc4aa4
Anna Shelby <bonanashelby@gmail.com>
2017-03-15 10:03:57 -0700

    added constructor function for portfolio and worked on more html

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var projects = [];
+
+// projects need a title, what language they are, the url and small description
+function Projects (projs) {
+  this.title = projs.title;
+  this.language = projs.language;
+  this.projectUrl = projs.projectUrl
+}
