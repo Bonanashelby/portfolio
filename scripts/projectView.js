5d5d57e3960628546a60142ae469d5c8d1f7a89f
Anna Shelby <bonanashelby@gmail.com>
2017-03-17 12:14:56 -0700

    added filters not populating titles or languages yet

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -2,6 +2,53 @@
 
 var projectView = {};
 
+projectView.populateFilters = function() {
+  $('project').each(function (){
+    if(!$(this).hasClass('template'))
+    {
+      var val = $(this).find('address a').text();
+      var optionTag = `<option value="${val}">${val}</option>`;
+
+      if ($(`#project-filter option[value="${val}"]`).length === 0){
+        $('#project-filter').append(optionTag);
+      }
+
+      val =
+      $(this).attr('data-category');
+      optionTag = `<option value="${val}">${val}</option>`;
+      if ($(`#category-filter option[value="${val}"]`).length === 0) {
+        $('#category-filter').append(optionTag);
+      }
+    }
+  });
+};
+
+projectView.handleTitleFilter = function() {
+  $('#title-filter').on('change', function() {
+    if ($(this).val()) {
+      $('project').hide();
+      $(`project[data-title="${$(this).val()}"]`).fadeIn();
+    } else {
+      $('project').fadeIn();
+      $('project.template').hide();
+    }
+    $('#language-filter').val('');
+  });
+};
+
+projectView.handleLanguageFilter = function () {
+  $('#language-filter').on('change', function () {
+    if ($(this).val() {
+      $('project').hide();
+      $(`project[data-language="${$(this).val()}"]`).fadeIn();
+    } else {
+      $('project').fadeIn();
+      $('project.template').hide();
+    }
+    $('project-filter').val('');
+  })
+}
+
 projectView.handleMainNav = function() {
   $('.main-nav').on('click', '.tab', function(event) {
     $('.tab-content').hide();
@@ -22,6 +69,9 @@ projectView.setDescriptionTeasers = function() {
 };
 
 $(document).ready(function(){
+  projectView.populateFilters();
+  projectView.handleLanguageFilter();
+  projectView.handleTitleFilter();
   projectView.handleMainNav();
   projectView.setDescriptionTeasers();
 })
