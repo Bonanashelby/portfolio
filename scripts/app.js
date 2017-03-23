e37470913c71888bd0759def54ff69659f9b8d5d
Anna Shelby <bonanashelby@gmail.com>
2017-03-23 13:34:25 -0700

    final commit before submit

--- scripts/app.js
+++ scripts/app.js
@@ -25,32 +25,32 @@ Projects.all = [];
 Projects.prototype.toHtml = function (){
   let template = Handlebars.compile($('#project-template').text());
 
-  this.daysAgo = parseInt((new Date () - new Date(this.completedOn))/60/6-/24/1000);
+  this.daysAgo = parseInt((new Date () - new Date(this.completedOn))/60/60/24/1000);
   this.completeStatus = this.completedOn ? `completed ${this.daysAgo} days ago` : '(draft)';
 
   return template(this);
 };
 // added to start getting ready for json
-Project.loadAll = function(projectData) {
+Projects.loadAll = function(projectData) {
   projectData.sort(function(a,b) {
-    return (new Date(b.completedOn)) - new Date(a.completedOn));
+    return (new Date(b.completedOn) - new Date(a.completedOn));
   });
 
   projectData.forEach(function(ele){
-    Project.all.push(new Project(ele));
+    Projects.all.push(new Projects(ele));
   })
 }
 // added to start getting ready for json
-Project.fetchAll = function() {
-  if (localStorage = projectData) {
-    Project.loadAll(JSON.parse(localStorage.prprojectData));
+Projects.fetchAll = function() {
+  if (localStorage.projectData) {
+    Projects.loadAll(JSON.parse(localStorage.projectData));
     projectView.initIndexPage();
   } else {
     $.getJSON('data/projectInfo.json')
     .then(function(data){
       localStorage.projectData = JSON.stringify(data);
-      Project.loadAll(data);
-      console.log(Project.all);
+      Projects.loadAll(data);
+      console.log(Projects.all);
       projectView.initIndexPage();
     }, function(err) {
         console.error(err);
